9decab774b460f71c2e7cd9448dc34291dceafc1
fofoMedrano23 <rod_medrano@hotmail.com>
2018-07-05 17:38:18 -0600

    fixed functionality for showing certain parts of form on grid

--- src/app/formularios/formulario1/formulario1.component.ts
+++ src/app/formularios/formulario1/formulario1.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Formulario1Component implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
--- src/app/reportes/reporte-solicitud-analisis/reporte-solicitud-analisis.component.ts
+++ src/app/reportes/reporte-solicitud-analisis/reporte-solicitud-analisis.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reporte-solicitud-analisis',
-  template: '<formio-grid src="https://ccovutxcvcdbcob.form.io/patientform1resource"><formio-grid>',
+  templateUrl: './reporte-solicitud-analisis.component.html',
   styleUrls: ['./reporte-solicitud-analisis.component.scss']
 })
 export class ReporteSolicitudAnalisisComponent implements OnInit {
