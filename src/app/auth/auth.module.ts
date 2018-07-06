73b626238890cfec97e087ff3b946999a98112d4
fofoMedrano23 <rod_medrano@hotmail.com>
2018-07-05 19:18:58 -0600

    fixed bug for login , when going back to login page the app tells them they are logged in and redirects

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormioAuth, FormioAuthRoutes } from 'angular-formio/auth';
+import { LoginComponent } from './login/login.component';
+import { FormioModule } from 'angular-formio';
+
+const authRoutes = FormioAuthRoutes({
+  login: LoginComponent
+});
 
 @NgModule({
   imports: [
     CommonModule,
     FormioAuth,
-    RouterModule.forChild(FormioAuthRoutes())
+    FormioModule,
+    RouterModule.forChild(authRoutes)
   ],
-  declarations: []
+  declarations: [LoginComponent]
 })
 export class AuthModule { }
